0a3a225347f3110740bbbefa1b8d3018f0a92c48
Jacob Mompean <jaymompean@gmail.com>
2022-09-13 23:26:50 -0500

    add programmatic navigation

--- file-based-routing/pages/clients/[id]/index.js
+++ file-based-routing/pages/clients/[id]/index.js
@@ -5,9 +5,18 @@ function ClientProjectsPage() {
 
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // load data...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'max', clientprojectid: 'project a' },
+    });
+  }
+
   return (
     <div>
       <h1>Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
