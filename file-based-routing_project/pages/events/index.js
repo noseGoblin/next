23cd1928cb6410cc8da73617aaa84b823cc64b0b
Jacob Mompean <jaymompean@gmail.com>
2022-09-18 22:01:13 -0500

    add main nav

--- file-based-routing_project/pages/events/index.js
+++ file-based-routing_project/pages/events/index.js
@@ -1,28 +1,9 @@
 import Link from 'next/link';
 
 function EventsPage() {
-  const events = [
-    { id: 1, name: 'first' },
-    { id: 2, name: 'second' },
-  ];
-
   return (
     <div>
       <h1>All Events</h1>
-      <ul>
-        {events.map((e) => (
-          <li key={e.id}>
-            <Link
-              href={{
-                pathname: 'events/[id]',
-                query: { id: e.id },
-              }}
-            >
-              {e.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
