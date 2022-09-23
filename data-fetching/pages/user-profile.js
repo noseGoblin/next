54106b465ac9f809e69b996adb77b45d15c34894
Jacob Mompean <jaymompean@gmail.com>
2022-09-22 21:46:54 -0500

    test log for server-side rendering

--- data-fetching/pages/user-profile.js
+++ data-fetching/pages/user-profile.js
@@ -7,6 +7,8 @@ export default UserProfilePage;
 export async function getServerSideProps(context) {
   const { params, req, res } = context;
 
+  console.log('Server side code');
+
   return {
     props: {
       username: 'Max',
