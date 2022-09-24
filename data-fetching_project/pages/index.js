6b293e1c845e0041a81e2fdad99be9990479ff9b
Jacob Mompean <jaymompean@gmail.com>
2022-09-23 22:47:20 -0500

    replace dumm data with getStaticProps for prerendered fetched data

--- data-fetching_project/pages/index.js
+++ data-fetching_project/pages/index.js
@@ -1,17 +1,25 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
-function Home() {
-  const featuredEvents = getFeaturedEvents();
-
+function Home(props) {
   return (
     <div>
       <main>
         <h1>Events Home</h1>
-        <EventList items={featuredEvents} />
+        <EventList items={props.events} />
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default Home;
