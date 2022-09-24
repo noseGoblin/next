2f67e9821809028bb83c57d8d05563c731c93e44
Jacob Mompean <jaymompean@gmail.com>
2022-09-24 13:24:18 -0500

    add header data to all events & events detail pages

--- optimizing-app/pages/events/[eventId].js
+++ optimizing-app/pages/events/[eventId].js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import Head from 'next/head';
 
 import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
@@ -20,6 +21,10 @@ function EventDetail(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>Event Page</title>
+        <meta name='description' content='Come join our event!' />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
