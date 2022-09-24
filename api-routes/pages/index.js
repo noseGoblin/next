de84a5fa25a9e6d830593961f2fed215348316fb
Jacob Mompean <jaymompean@gmail.com>
2022-09-24 14:46:45 -0500

    create form

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -2,6 +2,21 @@ function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <form action=''>
+        <div>
+          <label htmlFor='email' htmlFor='email'>
+            Your Eamil Address
+          </label>
+          <input type='email' id='email' />
+        </div>
+        <div>
+          <label htmlFor='feedback' htmlFor='email'>
+            Your Feedback
+          </label>
+          <textarea type='feedback' id='email'></textarea>
+          <button>Send Feedback</button>
+        </div>
+      </form>
     </div>
   );
 }
