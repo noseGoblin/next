44aa59440371d4b7a10f5b774174e93463ba63f8
Jacob Mompean <jaymompean@gmail.com>
2022-09-24 14:55:36 -0500

    add feedback routing to form

--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,19 +1,35 @@
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEamil = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
-      <form action=''>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor='email' htmlFor='email'>
             Your Eamil Address
           </label>
-          <input type='email' id='email' />
+          <input type='email' id='email' ref={emailInputRef} />
         </div>
         <div>
-          <label htmlFor='feedback' htmlFor='email'>
+          <label htmlFor='feedback' htmlFor='feedback'>
             Your Feedback
           </label>
-          <textarea type='feedback' id='email'></textarea>
+          <textarea
+            type='feedback'
+            id='feedback'
+            ref={feedbackInputRef}
+          ></textarea>
           <button>Send Feedback</button>
         </div>
       </form>
