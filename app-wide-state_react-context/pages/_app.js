05eee54124300cae164428376308eeb079e6d673
Jacob Mompean <jaymompean@gmail.com>
2022-09-30 18:24:35 -0500

    build notification handler & introduce to layout wrapper

--- app-wide-state_react-context/pages/_app.js
+++ app-wide-state_react-context/pages/_app.js
@@ -1,24 +1,24 @@
 import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
-import Notification from '../components/ui/notification';
+import { NotificationContextProvider } from '../store/notification-context';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NexJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-      <Notification
-        title='Test'
-        message='See my testing showing?'
-        status='error'
-      />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name='description' content='NexJS Events' />
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
