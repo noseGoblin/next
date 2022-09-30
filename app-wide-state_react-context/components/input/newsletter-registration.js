defd2da9176ffd51573df893b5ef6d93c8e5db7f
Jacob Mompean <jaymompean@gmail.com>
2022-09-30 18:37:46 -0500

    wire up newsletter registration notification context

--- app-wide-state_react-context/components/input/newsletter-registration.js
+++ app-wide-state_react-context/components/input/newsletter-registration.js
@@ -1,14 +1,22 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
+
+    notificationCtx.showNotification({
+      title: 'Singing up...',
+      messag: 'Registering for newsletter.',
+      status: 'pending',
+    });
 
     fetch('/api/newsletter', {
       method: 'POST',
@@ -17,8 +25,29 @@ function NewsletterRegistration() {
         'Content-Type': 'application/json',
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || 'DANGER: Something is wrong!');
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          messag: 'Thanks for registering for the newsletter!',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          messag: error.message || 'DANGER: Something is wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
--- app-wide-state_react-context/pages/api/newsletter.js
+++ app-wide-state_react-context/pages/api/newsletter.js
@@ -19,7 +19,7 @@ async function handler(req, res) {
     }
 
     try {
-      await insertDocument(client, 'newsletter' { email: userEmail });
+      await insertDocument(client, 'newsletter', { email: userEmail });
       client.close();
     } catch (error) {
       res.status(500).json({ message: 'Inserting data failed!' });
