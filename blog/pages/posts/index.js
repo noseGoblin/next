15ca6758292b812b0ddf1b5e88e82207fb6500b6
Jacob Mompean <jaymompean@gmail.com>
2022-10-02 17:10:42 -0500

    setup all posts page component w/ dummy data

--- blog/pages/posts/index.js
+++ blog/pages/posts/index.js
@@ -1,3 +1,38 @@
-function AllPostsPage() {}
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'test',
+    title: 'TEST Title',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'test excerpt',
+    date: '2022-10-02',
+  },
+  {
+    slug: 'test2',
+    title: 'TEST Title',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'test excerpt',
+    date: '2022-10-02',
+  },
+  {
+    slug: 'test3',
+    title: 'TEST Title',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'test excerpt',
+    date: '2022-10-02',
+  },
+  {
+    slug: 'test4',
+    title: 'TEST Title',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'test excerpt',
+    date: '2022-10-02',
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
