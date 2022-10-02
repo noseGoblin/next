cb7829d9104e949ef52087f47a3bcbbaefaec27d
Jacob Mompean <jaymompean@gmail.com>
2022-10-02 17:04:32 -0500

    add dummy data; setup blog grid and add to homepage/featured posts

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,12 +1,44 @@
 import { Fragment } from 'react';
 
+import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/hero';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'test',
+    title: 'TEST Title',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'test excerpt',
+    date: '2022-10-02',
+  },
+  {
+    slug: 'test2',
+    title: 'TEST Title',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'test excerpt',
+    date: '2022-10-02',
+  },
+  {
+    slug: 'test3',
+    title: 'TEST Title',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'test excerpt',
+    date: '2022-10-02',
+  },
+  {
+    slug: 'test4',
+    title: 'TEST Title',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'test excerpt',
+    date: '2022-10-02',
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
