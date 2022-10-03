6a7afae58c48bcf2f79f45dd6f58c37a3192e733
Jacob Mompean <jaymompean@gmail.com>
2022-10-02 23:05:41 -0500

    add markdown image rendering

--- blog/pages/posts/post-detail/post-content.js
+++ blog/pages/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -8,10 +9,23 @@ function PostConent(props) {
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    img(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
