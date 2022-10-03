4fef4689d346c3feec87567365d8a7eb5cbad1c2
Jacob Mompean <jaymompean@gmail.com>
2022-10-02 23:20:48 -0500

    replace markdown <img> with next <Image /> components

--- blog/pages/posts/post-detail/post-content.js
+++ blog/pages/posts/post-detail/post-content.js
@@ -10,15 +10,35 @@ function PostConent(props) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
-    img(image) {
-      return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
-        />
-      );
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
     },
   };
 
