c0389d3b8bfe149f505577c04d84dadf2f5ce6e1
Jacob Mompean <jaymompean@gmail.com>
2022-10-03 00:57:13 -0500

    connect to mongoDB & add post functionality & error handling

--- blog/pages/api/contact.js
+++ blog/pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -20,9 +22,37 @@ function handler(req, res) {
       name,
       message,
     };
-    console.log(newMessage);
 
-    res.status(201).json({ message: 'Success!', message: newMessage });
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://JacobM:9I0a8vupVsVi3Gip@cluster0.ryf9npw.mongodb.net/my-site?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Oh no! Something borked...' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.colleciton('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({
+        message: 'Sorry! Could not submit at this time.',
+      });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({
+      message: 'Success! We have received your message.',
+      message: newMessage,
+    });
   }
 }
 
