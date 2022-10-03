9a907f5521cf07ef94398b67dd2082d973e2ba37
Jacob Mompean <jaymompean@gmail.com>
2022-10-02 23:29:05 -0500

    add code syntax highlighting for presenting code snippets in markdown

--- blog/pages/posts/post-detail/post-content.js
+++ blog/pages/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -39,6 +41,19 @@ function PostConent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
