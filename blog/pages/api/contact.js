a6f08a2bbb182b3783c2ec110d936d8b341bfc21
Jacob Mompean <jaymompean@gmail.com>
2022-10-02 23:49:55 -0500

    add form validation & api route

---
+++ blog/pages/api/contact.js
@@ -0,0 +1,29 @@
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+
+    // Store data in a db
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+    console.log(newMessage);
+
+    res.status(201).json({ message: 'Success!', message: newMessage });
+  }
+}
+
+export default handler;
