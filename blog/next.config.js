a0f98dc5e29fe89697293b4fe979082c48402a18
Jacob Mompean <jaymompean@gmail.com>
2022-10-09 09:19:39 -0400

    separate dev & prod env variables

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,13 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  env: {
-    mongodb_username: 'JacobM',
-    mongodb_pw: '9I0a8vupVsVi3Gip',
-    mongodb_cluster: 'cluster0',
-    mongodb_database: 'my-site',
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'JacobM',
+        mongodb_pw: '9I0a8vupVsVi3Gip',
+        mongodb_cluster: 'cluster0',
+        mongodb_database: 'my-site-dev',
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: 'JacobM',
+      mongodb_pw: '9I0a8vupVsVi3Gip',
+      mongodb_cluster: 'cluster0',
+      mongodb_database: 'my-site',
+    },
+  };
 };
 
-module.exports = nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+//   swcMinify: true,
+// };
+
+// module.exports = nextConfig;
